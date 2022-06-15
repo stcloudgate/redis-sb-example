26fd8bc160e38d1fa149f92bc6c77ceee9e14661
Sanjeev Tiwari <stcloudgate@gmail.com>
2022-06-15 10:20:50 +0530

    initial-setup-1

---
+++ src/main/java/com/redis/sb/example/sbredisexample/SbredisexampleApplication.java
@@ -0,0 +1,13 @@
+package com.redis.sb.example.sbredisexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbredisexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbredisexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redis/sb/example/sbredisexample/SbredisexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis.sb.example.sbredisexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbredisexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
